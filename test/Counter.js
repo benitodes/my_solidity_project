4f7d902f3a370d870ff543760c95a568ce977399
benoit destresse <benoit@benoit-destresse.local>
2022-08-22 17:33:56 +0800

    add test for increment and decrement supply

--- test/Counter.js
+++ test/Counter.js
@@ -12,14 +12,39 @@ describe('Counter', () => {
         counter = await Counter.deploy('My counter', 1)
     })
 
-
-   it('stores the initial count', async () => {
-        expect(await counter.count()).to.equal(1)
+    describe('Deployment', () => {
+        it('stores the initial count', async () => {
+            expect(await counter.count()).to.equal(1)
+        })
+        it('sets the initial name', async () => {
+            expect(await counter.name()).to.equal('My counter')
+        })      
     })
 
-    it('sets the initial name', async () => {
-        expect(await counter.name()).to.equal('My counter')
+    describe('Counting', () => {
+        let transaction
+        
+        it('increments the count', async () => {
+            transaction = await counter.increment()
+            await transaction.wait()
+
+            expect(await counter.count()).to.equal(2)
+
+            transaction = await counter.increment()
+            await transaction.wait()
+
+            expect(await counter.count()).to.equal(3)
+        })
+
+        it('decrements the count', async () => {
+            transaction = await counter.decrement()
+            await transaction.wait()
+
+            expect(await counter.count()).to.equal(0)
+        })
+
+
+
+
     })
-
-
 })
